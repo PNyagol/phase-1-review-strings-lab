bcd9e78da807ddd22af1252a308560de7f140a44
Nyagol <nyagolpeter@gmail.com>
2023-10-24 07:44:41 +0300

    changes to the committ during practice

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
 // Write your code in this file!
+// const currentUser = 'Peter Nyagol';
+
+// const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+// const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+// const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
+
+
+
+// below is another way of doing this: just for practice purposes
+// I have used the .charAt() method here 
+
 const currentUser = 'Peter Nyagol';
-
 const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
-
 const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+const shortGreeting = `Welcome, ${currentUser.charAt(0)}!`;
 
-const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
+
+
+// next, I am going to use the bracket notation method
+
