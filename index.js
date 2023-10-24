ff9d57669b84c3e26d7220db46c8093472ecb457
Nyagol <nyagolpeter@gmail.com>
2023-10-24 07:47:35 +0300

    [] worked well

--- index.js
+++ index.js
@@ -15,9 +15,12 @@
 const currentUser = 'Peter Nyagol';
 const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
 const excitedWelcomeMessage = welcomeMessage.toUpperCase();
-const shortGreeting = `Welcome, ${currentUser.charAt(0)}!`;
+// const shortGreeting = `Welcome, ${currentUser.charAt(0)}!`;
 
 
 
 // next, I am going to use the bracket notation method
 
+const shortGreeting = `Welcome, ${currentUser[0]}!`
+
+// It worked as you can see above
